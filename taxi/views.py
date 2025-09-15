01e5e5576bf0382c8d3b2a179872aef0918b052e
Artem Shlychkin <artemshlychkin1995@gmail.com>
2025-09-15 08:22:41 +0300

    Solution.Implement new pages for car list view , car detail view, driver list view and driver detail view.Also implement paginations for each pages

--- taxi/views.py
+++ taxi/views.py
@@ -40,4 +40,5 @@ class CarDetailView(generic.DetailView):
 
 class DriverDetailView(generic.DetailView):
     model = Driver
-    queryset = Driver.objects.prefetch_related(Prefetch("cars", queryset=Car.objects.select_related("manufacturer")))
+    queryset = Driver.objects.prefetch_related(
+        Prefetch("cars", queryset=Car.objects.select_related("manufacturer")))
